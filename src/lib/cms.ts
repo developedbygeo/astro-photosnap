1a6bf46ff2445c82c7b06d3d1aecc43e8643c05d
developedbygeo <74512500+developedbygeo@users.noreply.github.com>
2025-01-05 21:48:37 +0200

    feat: add menu extraction and link splitting functions to enhance menu data handling

--- src/lib/cms.ts
+++ src/lib/cms.ts
@@ -1,7 +1,12 @@
 import type {
+  AcfLink,
+  CustomMenu,
+  CustomMenu_Menulinkscontent_MenuLinks,
+  Maybe,
   Page_Pagecontent_Content_FeaturedStories,
   Page_Pagecontent_Content_FeaturesSection,
 } from "@/__generated__/graphql";
+import type { WP_COMPONENT_IDS } from "@/data/misc";
 
 export const extractFeaturedStories = (
   stories?: Page_Pagecontent_Content_FeaturedStories,
@@ -19,4 +24,36 @@ export const extractTopFeatures = (
   if (!features || !features.featureRepeater) return [];
 
   return features.featureRepeater.map((feature) => feature?.content);
+};
+
+export const extractMenuById = (
+  allMenuData: CustomMenu[] | undefined,
+  menuDatabaseId: (typeof WP_COMPONENT_IDS)[keyof typeof WP_COMPONENT_IDS],
+) => {
+  const locatedMenu = allMenuData?.find(
+    (menu) => menu.databaseId === Number(menuDatabaseId),
+  );
+
+  return locatedMenu;
+};
+
+export const splitLinksByCtaStyling = (
+  links: Maybe<Maybe<CustomMenu_Menulinkscontent_MenuLinks>[]>,
+) => {
+  if (!links) return { ctaStyledLinks: [], regularLinks: [] };
+
+  return links?.reduce<{
+    ctaStyledLinks: AcfLink[];
+    regularLinks: AcfLink[];
+  }>(
+    (acc, link) => {
+      if (link && link.menuLink && link.enableCtaStyling === true) {
+        acc.ctaStyledLinks.push(link.menuLink);
+      } else if (link && link.menuLink) {
+        acc.regularLinks.push(link.menuLink);
+      }
+      return acc;
+    },
+    { ctaStyledLinks: [], regularLinks: [] },
+  );
 };
